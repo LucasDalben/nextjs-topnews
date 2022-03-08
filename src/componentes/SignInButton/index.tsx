955fd242c8249cd6717364ddccdb204f40c13736
LucasDalben <lucasdalbenn@gmail.com>
2022-03-08 00:07:47 -0300

    parte 3

--- src/componentes/SignInButton/index.tsx
+++ src/componentes/SignInButton/index.tsx
@@ -1,10 +1,28 @@
 import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+
+import styles from './style.module.scss';
 
 export function SignInButton() {
-    return (
-        <button type="button">
-            <FaGithub />
-            Sign in with Githhub
+
+    const isUserloggedIn = true;
+
+    return isUserloggedIn ? (
+        <button
+            type="button"
+            className={styles.signInButton}
+            >
+            <FaGithub color='#04d361' />
+            Lucas Dalbem
+            <FiX color='#737380' className={styles.closeIcon} /> 
         </button>
+    ) : (
+        <button
+            type="button"
+            className={styles.signInButton}
+            >
+            <FaGithub color='#eba417' />
+            Sign in with Githhub
+    </button>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,54 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 
-export default function Home() {
+import { SubscribeButton } from '../componentes/SubscribeButton';
+import { stripe } from '../services/stripe';
+import styles from './home.module.scss';
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+
   return (
     <>
       <Head>
-       <title>Topnews</title>
+        <title>Home  |  coffeenews</title>
       </Head>
-      <h1>
-      Hello World
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>☕ Hey, welcome!</span>
+          <h1>News about the <span>Coffee</span> World.</h1>
+          <p>
+            Get access to all the publications <br/>
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+        <img  src='/images/avatar.svg' alt='Girl coding'/>
+      </main>
     </>
-  
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KasjAFIH3AChXtpaiNvu4r8');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product
+    }
+  }
 }
