5f35e77fbc25daab80298f54e427bfd81abbfee6
LucasDalben <lucasdalbenn@gmail.com>
2022-03-07 22:15:06 -0300

    parte 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import { AppProps } from 'next/app';
+import { Header } from '../componentes/Header'
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header></Header>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
