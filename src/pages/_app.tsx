16559c595a6fd2332ced0160fcafef2a448295cc
LucasDalben <lucasdalbenn@gmail.com>
2022-03-10 08:41:48 -0300

    criado log in com o github, e autentificação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import { AppProps } from 'next/app';
 import { Header } from '../componentes/Header'
+import { Provider as NextAuthProvider } from 'next-auth/client';
+
 import '../styles/global.scss';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header></Header>
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
 
