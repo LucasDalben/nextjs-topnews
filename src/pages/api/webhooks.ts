101127a04922f813c4591a134697b047fedc7d07
LucasDalben <lucasdalbenn@gmail.com>
2022-03-24 22:53:52 -0300

    chunk

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,5 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Readable} from "stream";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+async function buffer(readable: Readable) {
+    const chunks = [];
+
+    for await (const chunk of readable) {
+        chunks.push(
+            typeof chunk === "string" ? Buffer.from(chunk) : chunk
+        );
+    }
+
+    return Buffer.concat(chunks);
+}
+
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const buf = await buffer(req);
+
     res.status(200).json({ ok: true })
 }
